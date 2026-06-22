9b75fe31f32b42c69fb876fd48063b5469cfd630
MinaroliCorentin <minaroli.corenin@gmail.com>
2026-06-22 03:32:03 +0200

    Remove CPU Bound on Benchmark because it's annoying to set it back

--- devtools/run_theorem_test.py
+++ devtools/run_theorem_test.py
@@ -2,7 +2,6 @@ import os
 import sys
 import re
 import time
-import subprocess
 from pathlib import Path
 from subprocess import PIPE, run
 
@@ -20,25 +19,12 @@ def LaunchTest(prover_name, command_line, succes, f, memory_limit=None, failure=
         f.write("Proof not found\n")
     return res
 
-def set_cpu_freq(freq_khz):
-    # Define CPU Speed 
-    cmd = f"sudo cpupower frequency-set -g performance -u {freq_khz} -d {freq_khz}"
-    result = subprocess.run(cmd, shell=True, stdout=PIPE, stderr=PIPE, universal_newlines=True)
-    if result.returncode != 0:
-        print("Error. sudo ?.")
-        print(result.stderr)
-    else:
-        print(f"CPU Speed is {int(freq_khz)/1000} MHz.")
 
 if len(sys.argv) < 3:
     print(f"python3 {sys.argv[0]} problem_folder timeout goeland_options")
 else:
     NB_CORES = 4
     CORES = ",".join(str(i) for i in range(NB_CORES))
-    FREQ_KHZ = 2000000  # 2.0 GHz
-
-    set_cpu_freq(FREQ_KHZ)
-    print(f"{NB_CORES} cores are working")
 
     folder = sys.argv[1]
     folder_split = folder.split("/")
@@ -48,10 +34,10 @@ else:
     total = len(entries)
 
     filename = ""
-    for i in range (0,sys.maxsize) : 
-        a  = Path("resultV1_" + str(i) + ".txt")
-        if not a.exists(): 
-            filename = a 
+    for i in range(0, sys.maxsize):
+        a = Path("resultV1_" + str(i) + ".txt")
+        if not a.exists():
+            filename = a
             break
 
     with open(filename, "w") as f:
@@ -61,7 +47,7 @@ else:
             f.write(f"Problem {index+1}/{len(entries)} : {folder+file}\n")
             command = (
                 f"taskset -c {CORES} env GOMAXPROCS={NB_CORES} "
-                f"timeout {timeout} ../src/_build/goeland -dt "
+                f"timeout {timeout} ../src/_build/goeland "
                 + " ".join(sys.argv[4:]) + " " + folder + file
             )
             if LaunchTest("Goéland", command, "% RES : VALID", f, None, "% RES : NOT VALID"):
